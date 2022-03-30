c67a70cd98c6e88e77577395274c341bcaf755dc
Maryia Kalodkina <60666264+maryiakalodkina@users.noreply.github.com>
2022-03-30 17:51:52 -0400

    Add files via upload

--- Task4__add_integers_in_matrix_spiral.py
+++ Task4__add_integers_in_matrix_spiral.py
@@ -2,7 +2,7 @@
 # Add all the integers in the matrix 4x4 in a spiral form starting with the first column
 
 
-def add_matrix(matrix, r, c):
+def add_matrix(matrix):
 
     ''' f - first index of the raw
         l - last index of the raw
@@ -11,8 +11,10 @@ def add_matrix(matrix, r, c):
     
     f = 0
     s = 0
-    l = c
-    t = r
+    l = len(matrix[0])
+    t = len(matrix)
+
+    print('l is {} and t is {}'.format(l,t))
 
     sum = 0
 
@@ -24,14 +26,12 @@ def add_matrix(matrix, r, c):
             sum += matrix[i][f]
         f+=1
 
-        print()
         #traversing the last raw
         for i in range(f, l):
             print(matrix[t-1][i])
             sum += matrix[t-1][i]
         t-=1
 
-        print()
         #traversing the last column
         if(f<l):
             for i in range(t-1, s-1, -1):
@@ -39,14 +39,12 @@ def add_matrix(matrix, r, c):
                 sum += matrix[i][(l-1)]
             l-=1
         
-        print()
         #traversing the first raw
         if(s<t):
             for i in range(l-1, (f-1), -1):
                 print(matrix[s][i])
                 sum += matrix[s][i]
             s+=1
-        print()
 
     return sum
 
@@ -54,13 +52,11 @@ new_matrix = [[2, 3, 4, 5], [14, 8, 11, 9], [3, 14, 48, 5], [23, 29, 4, 6]] # 4x
 
 ''' 
     2           3   <-    4    <-   5
-                                    ^
-    |           |                   |
-    v           v
+    |           |                   ^
+    v           v                   |
     14          8        11         9
-                         ^          ^
-    |           |        |          |
-    v           v
+    |           |        ^          ^
+    v           v        |          |             
     3           14   ->  48         5
     |                               ^
     v                               |
@@ -68,7 +64,6 @@ new_matrix = [[2, 3, 4, 5], [14, 8, 11, 9], [3, 14, 48, 5], [23, 29, 4, 6]] # 4x
     
     '''
 
-r = 4
-c = 4
-answer = add_matrix(new_matrix, r, c)
+
+answer = add_matrix(new_matrix)
 print(answer)
