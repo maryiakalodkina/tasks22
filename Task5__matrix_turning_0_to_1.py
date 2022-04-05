f9191a4bf9895fcbc90c8bb47043b0e0d20c3d22
Maryia Kalodkina <60666264+maryiakalodkina@users.noreply.github.com>
2022-04-04 22:26:02 -0400

    Add files via upload

--- Task5__matrix_turning_0_to_1.py
+++ Task5__matrix_turning_0_to_1.py
@@ -3,12 +3,12 @@
 
 def turning_zeros_to_ones(zero_matrix):
 
-    ''' f - first index of the raw
-        c - last index of the raw
+    ''' f - first index of the row
+        c - last index of the row
         s - first index of the column
         r - last index of the column'''
 
-    #how many raws
+    #how many rows
     r = len(zero_matrix)
     #how many columns
     c = len(zero_matrix[0])
@@ -16,42 +16,59 @@ def turning_zeros_to_ones(zero_matrix):
     f = 0
     s = 0
 
-    #new matrix we will return
-    one_matrix = [[0 for col in range(c)] for raw in range(r)]
+    array_rows = []
+    array_cols = []
     
     print('Initial matrix:')
     for ch in range(s, r):
         print(zero_matrix[ch])
     
-    #goint through all the raws
+# STEP 1: Figuring out what rows and columns have to be turned to 1s
+
+    #goint through all the rows
     for i in range(s, r):
         #goint through all the columns
         for j in range(f, c):
             #if we see any 1s
             if zero_matrix[i][j] == 1:
-                #number of the raw to be turned to 1
-                raw = i
+                #number of the row to be turned to 1
+                row = i
                 #number of the column to be turned to 1
                 column = j
 
-                #turn the whole raw i to ones in one_matrix
+                array_rows.append(i)
+                array_cols.append(j)
+
+    #removing dublicates in both and sorting
+    rows = sorted(list(set(array_rows)))          
+    columns = sorted(list(set(array_cols)))        
+
+    point_r = 0
+    point_c = 0
+
+    # STEP 2: Turning specific rows and columns to 1s
+
+    #goint through all the rows
+    for k in range(s, r):
+        #goint through all the columns
+        for l in range(f, c):
+            if ((point_r < (len(rows)) and (k == rows[point_r]))):
+                #turn the whole row k to ones in zero_matrix
                 for m in range(f, c):
-                    one_matrix[i][m] = 1
-                #turn the whole raw i to ones in one_matrix
+                    zero_matrix[k][m] = 1
+                point_r += 1
+            if  (point_c < (len(columns))) and ((l == columns[point_c])):
+            #turn the whole column l to ones in zero_matrix
                 for n in range(s, r):
-                    one_matrix[n][j] = 1
-    
+                    zero_matrix[n][l] = 1     
+                point_c += 1          
+
     print()
     print('New matrix:')
     for th in range(s, r):
-        print(one_matrix[th])
+        print(zero_matrix[th])
 
-    return one_matrix
-
+    return zero_matrix
 
 new_matrix = [[0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1]]
 answer = turning_zeros_to_ones(new_matrix)
-
-
-
-        
