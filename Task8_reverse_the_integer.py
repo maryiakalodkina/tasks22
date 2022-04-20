073aafe7b5dfbc69c975c04f165ecc7d90633009
Maryia <mk70123n@pace.edu>
2022-04-19 22:49:59 -0400

    Updated task8 with 4 different solutions

--- Task8_reverse_the_integer.py
+++ Task8_reverse_the_integer.py
@@ -1,12 +1,56 @@
-def reverse_int(my_int):
-    output = 0
-    while (my_int > 0):
-        output = (output * 10) + (my_int % 10)
-        my_int = (my_int // 10)
-  
+# TASK 8: reverse a positive integer
 
-    return output
+#FIRST APPROACH:
+#through list
+def reverseIntFirstApproach(givenInt):
+    
+    #converting int to list of strings:
+    result = [str(x) for x in str(givenInt)]
+    #reversing the list
+    result.reverse()
+    #combining the elements of list in one string and turning it back to int
+    result = int(''.join(result))
+    
+    return result
+
+#SECOND APPROACH:
+#through string
+def reverseIntSecondApproach(givenInt):
+
+    #converting int to a string, reversing it and turning back to int
+    result2 = int(''.join(reversed(str(givenInt))))
+    
+    return result2
+
+#THIRD APPROACH:
+# mathematical approach
+def reverseIntThirdApproach(givenInt):
+
+    result3 = 0
+    while (givenInt > 0):
+        result3 = (result3 * 10) + (givenInt % 10)
+        givenInt = (givenInt // 10)
+
+    return result3
+
+#FORTH APPROACH:
+#mathematical with recursion 
+def reverseIntFourthApproach(givenInt, result4):
+    
+    result4 = (result4 * 10) + (givenInt % 10)
+    givenInt = (givenInt // 10)
+
+    # print('givenInt is {} and result4 is {}'.format(givenInt, result4))
+
+    if givenInt > 0:
+        return reverseIntFourthApproach(givenInt, result4)
+    else:
+        # print(result4)
+        return result4
+
 
 new_int = 12345
-output = reverse_int(new_int)
-print(output)
+answer = reverseIntFirstApproach(new_int)
+answer2 = reverseIntSecondApproach(new_int)
+answer3 = reverseIntThirdApproach(new_int)
+answer4 = reverseIntFourthApproach(new_int, 0)
