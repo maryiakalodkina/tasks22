334aa442b01b36ed3abf3a46875e4485cbd825e2
Maryia <mk70123n@pace.edu>
2022-05-25 22:42:06 -0400

    Updated task 13 again

--- Task13_LeetCode_task56_mergeIntervals.py
+++ Task13_LeetCode_task56_mergeIntervals.py
@@ -1,11 +1,11 @@
-# LC task 56
+# LeetCode task 56
 
 def merge(intervals):
         intervals.sort()
-        mergedIntervals = []
         allMerged = False
         skip = False
         while allMerged != True:
+            mergedIntervals = []
             for i in range(0, len(intervals)):
                 if skip:
                     skip = False
@@ -19,7 +19,6 @@ def merge(intervals):
                     else:
                         temp.append(intervals[i+1][1])
                     mergedIntervals.append(temp) 
-                   
                 else:
                     mergedIntervals.append(intervals[i])
             if intervals == mergedIntervals:
@@ -27,9 +26,14 @@ def merge(intervals):
             else:
                 intervals.clear()
                 intervals+=mergedIntervals
-                mergedIntervals.clear()
         return intervals     
 
-intervals = [[1,3],[2,6],[8,10],[15,18]]
-answer = merge(intervals)
+intervals1 = [[1,3],[2,6],[8,10],[15,18]] #[[1,6],[8,10],[15,18]]
+intervals2 = [[1,4],[4,5]] #[[1,5]]
+intervals3 = [[1,4],[0,4]] #[[0,4]]
+intervals4 = [[1,4],[2,3]] #[[1,4]]
+intervals5 = [[2,3],[4,5],[6,7],[8,9],[1,10]] #[[1,10]]
+intervals6 = [[2,3],[5,5],[2,2],[3,4],[3,4]] #[[2,4],[5,5]]
+
+answer = merge(intervals6)
 print(answer)
