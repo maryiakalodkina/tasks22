d83fd4c17e1298c377816f2a34dfa95dd2bf51ce
Maryia <mk70123n@pace.edu>
2022-05-25 22:18:45 -0400

    Adding task13 - LeetCode #56

---
+++ Task13_LeetCode_task56_mergeIntervals.py
@@ -0,0 +1,40 @@
+# LC task 56
+
+def merge(intervals):
+        intervals.sort()
+        mergedIntervals = []
+        allMerged = False
+        skip = False
+        while allMerged != True:
+            for i in range(0, len(intervals)):
+                if skip:
+                    skip = False
+                    pass
+                elif i+1 < len(intervals) and intervals[i+1][0] <= intervals[i][1]:
+                    temp = []
+                    if intervals[i+1][1] < intervals[i][1]:
+                        temp.append(intervals[i][0])
+                        temp.append(intervals[i][1])
+                        skip = True
+                    elif intervals[i+1][1] == intervals[i][1]:
+                        temp.append(intervals[i][0])
+                        temp.append(intervals[i][1])
+                        skip = True
+                    else:
+                        temp.append(intervals[i][0])
+                        temp.append(intervals[i+1][1])
+                    mergedIntervals.append(temp) 
+                   
+                else:
+                    mergedIntervals.append(intervals[i])
+            if intervals == mergedIntervals:
+                allMerged = True
+            else:
+                intervals.clear()
+                intervals+=mergedIntervals
+                mergedIntervals.clear()
+        return intervals       
+
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+answer = merge(intervals)
+print(answer)
